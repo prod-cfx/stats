5beaa8d9110c3e678eb44be2d596679b9bef04d0
zq <ahmmzxf@gmail.com>
2025-12-25 09:28:30 +0800

    chore: remove duplicate migration directory

--- apps/backend/prisma/schema/migrations/20251224221100_add_unique_market_config_and_instrument_index/migration.sql
+++
@@ -1,5 +0,0 @@
--- Add unique constraint on (symbol, venue, instrument_type)
-CREATE UNIQUE INDEX "unique_market_config" ON "orderbook_pair_configs"("symbol", "venue", "instrument_type");
-
--- Add index on instrument_type for better query performance
-CREATE INDEX "orderbook_pair_configs_instrument_type_idx" ON "orderbook_pair_configs"("instrument_type");
